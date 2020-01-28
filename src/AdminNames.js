da5a11c2b2a6ba90986a5f04f8f90542eb97e0d7
Presentations <jamierobertdawson@gmail.com>
2020-01-27 21:00:30 -0800

    updated with new code to handle multiple admin names

--- src/AdminNames.js
+++ src/AdminNames.js
@@ -1,13 +1,22 @@
-async function _command(params, text, secrets = {}) {
-    const {
-      msg
-    } = params;
-  
-    return {
-      response_type: 'in_channel', // or `ephemeral` for private response
-      text: "Jamie Test here!!!"
-    };
-  }
-  
-  const main = async ({__secrets = {}, text, ...params}) => ({body: await _command(params, text, __secrets)});
-  module.exports = main;
+async function _command(text, secrets = {}) {
+    var names = [
+       "@Jamie Dawson", 
+       "@Test_Admin_101",
+       "@Test_Admin_202"
+    ]
+      
+    var fullSentence = "`Admins you can contact are :`"
+    var x = names.toString();
+    
+    for (var i = 0; i < names.length; i++){
+       fullSentence = fullSentence.concat(names[i]).concat(" ");
+    }
+    
+      return {
+        response_type: 'in_channel', // or `ephemeral` for private response
+        text: fullSentence
+      };
+    }
+    
+    const main = async ({__secrets = {}, text, ...params}) => ({body: await _command(params, text, __secrets)});
+    module.exports = main;
